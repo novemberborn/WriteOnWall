fd9eb0fca42e0227e40c333aad27b68a6b3a9a38
Dirk van Oosterbosch <dirk@ixopusada.com>
2010-11-20 19:59:05 +0000

    adding the of project
    git-svn-id: svn://svn.mediamatic.nl/devcamps/camp10/wow@1288 76a45c34-74ab-11dd-89ba-81adbf74ab27

---
+++ of/devcamps_OpenCV_example/src/testApp.cpp
@@ -0,0 +1,198 @@
+#include "testApp.h"
+
+
+//--------------------------------------------------------------
+void testApp::setup(){
+
+
+	#ifdef _USE_LIVE_VIDEO
+        vidGrabber.setVerbose(true);
+        vidGrabber.initGrabber(320,240);
+	#else
+        vidPlayer.loadMovie("fingers.mov");
+        vidPlayer.play();
+	#endif
+
+    colorImg.allocate(320,240);
+	hsbImg.allocate(320,240);
+	
+	hueImg.allocate(320,240);
+	satImg.allocate(320,240);
+	briImg.allocate(320,240);
+	
+	rImg.allocate(320,240);
+	gImg.allocate(320,240);
+	bImg.allocate(320,240);
+	
+	grayImage.allocate(320,240);
+	grayBg.allocate(320,240);
+	grayDiff.allocate(320,240);
+	
+	rgbComposite = cvCreateImage(cvSize(320, 240), IPL_DEPTH_8U, 3);
+	brushImg.allocate(320,240);
+
+	bLearnBakground = true;
+	threshold = 80;
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	ofBackground(100,100,100);
+
+    bool bNewFrame = false;
+
+	#ifdef _USE_LIVE_VIDEO
+       vidGrabber.grabFrame();
+	   bNewFrame = vidGrabber.isFrameNew();
+    #else
+        vidPlayer.idleMovie();
+        bNewFrame = vidPlayer.isFrameNew();
+	#endif
+
+	if (bNewFrame){
+
+		#ifdef _USE_LIVE_VIDEO
+            colorImg.setFromPixels(vidGrabber.getPixels(), 320,240);
+	    #else
+            colorImg.setFromPixels(vidPlayer.getPixels(), 320,240);
+        #endif
+		
+		hsbImg = colorImg;
+		hsbImg.convertRgbToHsv();
+		hsbImg.convertToGrayscalePlanarImages(hueImg, satImg, briImg);
+		
+
+        grayImage = briImg;
+		if (bLearnBakground == true){
+			grayBg = grayImage;		// the = sign copys the pixels from grayImage into grayBg (operator overloading)
+			bLearnBakground = false;
+		}
+
+		// take the abs value of the difference between background and incoming and then threshold:
+		grayDiff.absDiff(grayBg, grayImage);
+		grayDiff.threshold(threshold);
+		
+		//
+		
+		colorImg.convertToGrayscalePlanarImages(rImg, gImg, bImg);
+		//cv
+		cvCopy(colorImg.getCvImage(), rgbComposite, grayDiff.getCvImage());
+		//cvMerge(rImg.getCvImage(), gImg.getCvImage(), bImg.getCvImage(), grayDiff.getCvImage(), rgbComposite);
+		brushImg = rgbComposite;
+
+		/*
+		// find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
+		// also, find holes is set to true so we will get interior contours as well....
+		contourFinder.findContours(grayDiff, 20, (340*240)/3, 10, true);	// find holes
+		*/
+	}
+
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+
+	// draw the incoming, the grayscale, the bg and the thresholded difference
+	ofSetColor(0xffffff);
+	
+#ifdef _USE_LIVE_VIDEO
+
+	brushImg.draw(0,0, 1024, 768);
+
+#else
+
+	colorImg.draw(0,0);
+	
+	hueImg.draw(640, 0);
+	satImg.draw(640, 240);
+	briImg.draw(640, 480);
+	
+	grayImage.draw(320,0);
+	grayBg.draw(0,240);
+	grayDiff.draw(320,240);
+	
+	brushImg.draw(320,480);
+	
+#endif
+	
+
+	/*
+	 // then draw the contours:
+	 
+	ofFill();
+	ofSetColor(0x333333);
+	ofRect(320,480,320,240);
+	ofSetColor(0xffffff);
+
+	// we could draw the whole contour finder
+	//contourFinder.draw(360,540);
+
+	// or, instead we can draw each blob individually,
+	// this is how to get access to them:
+    for (int i = 0; i < contourFinder.nBlobs; i++){
+        contourFinder.blobs[i].draw(320,480);
+    }
+	 
+	
+	// finally, a report:
+
+	ofSetColor(0xffffff);
+	char reportStr[1024];
+	sprintf(reportStr, "bg subtraction and blob detection\npress ' ' to capture bg\nthreshold %i (press: +/-)\nnum blobs found %i, fps: %f", threshold, contourFinder.nBlobs, ofGetFrameRate());
+	ofDrawBitmapString(reportStr, 20, 600);
+	 */
+
+}
+
+
+//--------------------------------------------------------------
+void testApp::keyPressed  (int key){
+
+	switch (key){
+		case ' ':
+			bLearnBakground = true;
+			break;
+		case '-':
+			threshold ++;
+			if (threshold > 255) threshold = 255;
+			break;
+		case '_':
+			threshold ++;
+			if (threshold > 255) threshold = 255;
+			break;
+		case '=':
+			threshold --;
+			if (threshold < 0) threshold = 0;
+			break;
+		case '+':
+			threshold --;
+			if (threshold < 0) threshold = 0;
+			break;
+		case 's':
+			vidGrabber.videoSettings();
+			break;
+	}
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
---
+++ of/devcamps_OpenCV_example/src/testApp.h
@@ -0,0 +1,60 @@
+#ifndef _TEST_APP
+#define _TEST_APP
+
+#include "ofMain.h"
+
+#include "ofxOpenCv.h"
+
+#define _USE_LIVE_VIDEO		// uncomment this to use a live camera
+								// otherwise, we'll use a movie file
+
+//#define _FULLSCREEN
+
+class testApp : public ofBaseApp{
+
+	public:
+
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+
+        #ifdef _USE_LIVE_VIDEO
+		  ofVideoGrabber 		vidGrabber;
+		#else
+		  ofVideoPlayer 		vidPlayer;
+		#endif
+
+		ofxCvColorImage		colorImg, hsbImg;
+
+		ofxCvGrayscaleImage		hueImg;
+		ofxCvGrayscaleImage		satImg;
+		ofxCvGrayscaleImage		briImg;
+
+		ofxCvGrayscaleImage		rImg;
+		ofxCvGrayscaleImage		gImg;
+		ofxCvGrayscaleImage		bImg;
+	
+        ofxCvGrayscaleImage 	grayImage;
+		ofxCvGrayscaleImage 	grayBg;
+		ofxCvGrayscaleImage 	grayDiff;
+	
+		IplImage		*rgbComposite;
+		ofxCvColorImage		brushImg;
+	
+
+        //ofxCvContourFinder 	contourFinder;
+
+		int 				threshold;
+		bool				bLearnBakground;
+
+
+};
+
+#endif
