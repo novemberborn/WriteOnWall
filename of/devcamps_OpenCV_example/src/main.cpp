66ae9f26c162644820ecffa3a31bb6daebc3bb9f
Dirk van Oosterbosch <dirk@ixopusada.com>
2010-11-26 22:52:00 +0000

    The OF app can now be placed in the exact location necessary.[[br]] Change the `_WINDOW_X` and `_WINDOW_Y` values in testApp.h to position the window correctly
    git-svn-id: svn://svn.mediamatic.nl/devcamps/camp10/wow@1625 76a45c34-74ab-11dd-89ba-81adbf74ab27

--- of/devcamps_OpenCV_example/src/main.cpp
+++ of/devcamps_OpenCV_example/src/main.cpp
@@ -8,6 +8,9 @@ int main( ){
     ofAppGlutWindow window;
 	int totalScreenWidth = 1024 + _SCREEN_WIDTH;
 	ofSetupOpenGL(&window, totalScreenWidth ,768, OF_WINDOW);			// <-------- setup the GL context
+	
+	// Set the position of the window:
+	window.setWindowPosition(_WINDOW_X, _WINDOW_Y);
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
